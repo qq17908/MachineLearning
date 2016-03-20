7c8515da7d46a4be029601244b20d5b8180581ce
qq17908 <vippopo@163.com>
2016-03-21 07:25:22 +0800

    add mind

---
+++ 数据分析思考.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0000ff" CREATED="1458213990246" ID="Freemind_Link_1380993940" MODIFIED="1458310902219" TEXT="&#x6570;&#x636e;&#x5206;&#x6790;&#x3001;&#x6316;&#x6398;">
+<node CREATED="1458308555252" ID="Freemind_Link_558706640" MODIFIED="1458309530105" POSITION="right" TEXT="&#x6570;&#x636e;&#x5206;&#x6790;">
+<icon BUILTIN="idea"/>
+</node>
+<node CREATED="1458308995307" ID="Freemind_Link_814942776" MODIFIED="1458311581111" POSITION="left" TEXT="&#x8bc1;&#x5238;&#x77e5;&#x8bc6;">
+<attribute_layout NAME_WIDTH="45" VALUE_WIDTH="45"/>
+</node>
+<node CREATED="1458309577847" ID="Freemind_Link_1764856793" MODIFIED="1458309656225" POSITION="right" TEXT="&#x5de5;&#x5177;&#x7bc7;">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1458308492699" ID="_" MODIFIED="1458308499358" TEXT="Python"/>
+<node CREATED="1458309593430" ID="Freemind_Link_1888385775" MODIFIED="1458309596042" TEXT="Excel"/>
+<node CREATED="1458311420695" ID="ID_941039803" LINK="Git.mm" MODIFIED="1458311420698" TEXT="Git"/>
+<node CREATED="1458309626099" ID="Freemind_Link_706027543" MODIFIED="1458309630359" TEXT="Matlab"/>
+<node CREATED="1458309597790" ID="Freemind_Link_447697092" MODIFIED="1458309602782" TEXT="Linux"/>
+<node CREATED="1458309612949" ID="Freemind_Link_207155488" MODIFIED="1458309616573" TEXT="tushare"/>
+</node>
+<node CREATED="1458309685381" ID="Freemind_Link_1220832115" MODIFIED="1458309691729" POSITION="left" TEXT="&#x5206;&#x6790;&#x4ec0;&#x4e48;&#x6570;&#x636e;&#xff1f;">
+<node CREATED="1458308538429" ID="Freemind_Link_1779845718" MODIFIED="1458308546397" TEXT="&#x91cf;&#x5316;&#x4ea4;&#x6613;"/>
+<node CREATED="1458308638292" ID="Freemind_Link_1600238062" MODIFIED="1458308642317" TEXT="&#x8d22;&#x52a1;&#x7ba1;&#x7406;"/>
+</node>
+<node CREATED="1458309717274" ID="Freemind_Link_422583267" MODIFIED="1458309897578" POSITION="right" TEXT="&#x56fe;&#x4e66;&#x7b14;&#x8bb0;">
+<icon BUILTIN="password"/>
+<node CREATED="1458309723141" ID="Freemind_Link_480006582" MODIFIED="1458309738858" TEXT="&#x300a;&#x5229;&#x7528;Python&#x8fdb;&#x884c;&#x6570;&#x636e;&#x5206;&#x6790;&#x300b;"/>
+<node CREATED="1458309739424" ID="Freemind_Link_1570566797" MODIFIED="1458309751580" TEXT="&#x300a;&#x6df1;&#x5165;&#x6d45;&#x51fa;&#x6570;&#x636e;&#x5206;&#x6790;&#x300b;"/>
+<node CREATED="1458309847988" ID="Freemind_Link_1702720753" MODIFIED="1458309855633" TEXT="&#x300a;&#x6df1;&#x5165;&#x6d45;&#x51fa;&#x7edf;&#x8ba1;&#x5b66;&#x300b;"/>
+</node>
+</node>
+</map>
