d6d54485ceacf68630953173b90637430e0194c2
qq17908 <vippopo@163.com>
2016-04-12 08:19:22 +0800

    Signed-off-by: qq17908 <vippopo@163.com>

--- 《深入浅出数据分析》.mm
+++ 《深入浅出数据分析》.mm
@@ -1,4 +1,4 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1458309739424" LINK="&#x6570;&#x636e;&#x5206;&#x6790;&#x601d;&#x8003;.mm" MODIFIED="1460280400686" TEXT="&#x300a;&#x6df1;&#x5165;&#x6d45;&#x51fa;&#x6570;&#x636e;&#x5206;&#x6790;&#x300b;"/>
+<node CREATED="1458309739424" ID="ID_247245690" LINK="&#x6570;&#x636e;&#x5206;&#x6790;&#x601d;&#x8003;.mm" MODIFIED="1460280400686" TEXT="&#x300a;&#x6df1;&#x5165;&#x6d45;&#x51fa;&#x6570;&#x636e;&#x5206;&#x6790;&#x300b;"/>
 </map>
